d27bedd6df0c5fa45efe730b93b960abeda09d92
Mohamed fadel Labrayji <mlabrayj@e2r4p12.1337.ma>
2021-04-15 13:06:46 +0000

    allright

--- srcs/MySQL/image/start.sh
+++ srcs/MySQL/image/start.sh
@@ -3,10 +3,10 @@ openrc reboot
 service mariadb start
 mysql -e "CREATE USER 'mlabrayj'@'%' IDENTIFIED BY 'password';"
 mysql -e "CREATE DATABASE wordpress;"
-mysql < ./wordpress.sql -u root wordpress; 
+mysql < ./wordpress.sql -u root wordpress;
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'mlabrayj'@'%';"
 mysql -e "FLUSH PRIVILEGES"
-# mysql -e "./mlabrayj_db.sql -u mlabrayj"
+mysql  < ./phpmyadmin.sql -u root phpmyadmin;
 service mariadb restart
 /telegraf/telegraf &
 tail -f /dev/null
